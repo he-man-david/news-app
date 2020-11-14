1091df484290bbfba36888bf17f48e26b3f64330
david he man <david.he.is.here@gmail.com>
2020-11-14 18:08:40 -0500

    setting up workspace

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import NavBar from './components/navbar/navbar';
+import StockButler from './components/stockButler/stockButler'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {  }
+    render() { 
+        return (
+            <Container>
+                <Router>
+                    <Row><Col><StockButler></StockButler></Col></Row>
+                    <Row><Col><NavBar></NavBar></Col></Row>
+                </Router>
+            </Container>
+        );
+    }
 }
-
+ 
 export default App;
