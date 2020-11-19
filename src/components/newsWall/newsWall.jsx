ba19d7b554bbab38c859fcbc53d539cec990f069
david he man <david.he.is.here@gmail.com>
2020-11-19 11:30:00 -0500

    converted NewsWall to functional! functional programming rocks!

--- src/components/newsWall/newsWall.jsx
+++ src/components/newsWall/newsWall.jsx
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import './newsWall.css';
 import SearchBar from '../searchBar/searchBar';
+import NewsItem from '../newsItem/newsItem';
 
-class NewsWall extends Component {
-    state = {  }
-    render() { 
-        return (
-            <div id="newsWallMain">
-                <SearchBar></SearchBar>
-                <h1>This is a news Wall</h1>
-                <p>fewfwef</p>
-            </div>
-        );
+function NewsWall({ routeName }) {
+    const [newsList, setNewsList] = React.useState([]);
+
+    React.useEffect(() => {
+        console.log("routeName has changed ---> ", routeName);
+        // return () => {
+        //     cleanup
+        // };
+    }, [routeName]);
+
+    const handleSearch = search => {
+        // handle search API here
+        alert("Search this ----> ", search);
     }
+
+    return (
+        <div id="newsWallMain">
+            <SearchBar handleSearch={handleSearch}></SearchBar>
+            <div>
+                {newsList.map(newsItem => {
+                    return <newsItem newsItem={newsItem}></newsItem>
+                })}
+            </div>
+        </div>
+    );
 }
  
 export default NewsWall;
